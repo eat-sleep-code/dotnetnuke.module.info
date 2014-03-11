bebe73c83f380fc6c6f388cc08701ddb94db4ee8
Eat Sleep Code <info@eat-sleep-code.com>
2014-03-11 09:31:16 -0700

    Initial Commit

---
+++ App_Code/base.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using DotNetNuke;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Controllers;
+using DotNetNuke.Framework.Providers;
+
+public class Module_Info : DotNetNuke.Entities.Modules.PortalModuleBase
+{
+	public void get_info(string AuthKey, int ModuleID = 0, int TabID = 0)
+	{
+
+		if (AuthKey.Trim().ToLower() == "true") 
+		{
+			StringBuilder str_debug = new StringBuilder();
+			str_debug.AppendLine("<table>");
+			str_debug.AppendLine("<tr><td>Version:</td><td>" + "" + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Data Provider:</td><td>" +  ProviderConfiguration.GetProviderConfiguration("data").DefaultProvider + "</td></tr>");
+			str_debug.AppendLine("<tr><td>.NET Framework:</td><td>" + Environment.Version.ToString() + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Identity:</td><td>" + WindowsIdentity.GetCurrent().Name + "</td></tr>");
+			str_debug.AppendLine("<tr><td>DNS Host Name:</td><td>" + Dns.GetHostName() + "</td></tr>");
+			str_debug.AppendLine("<tr><td>SMTP Server:</td><td>" + HostController.Instance.GetString("SMTPServer") + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Performance Setting:</td><td>" + HostController.Instance.GetString("PerformanceSetting") + "</td></tr>");
+			str_debug.AppendLine("<tr><td colspan=\"2\"><hr></td></tr>");
+			str_debug.AppendLine("<tr><td>Host Title:</td><td>" + HostController.Instance.GetString("HostTitle") + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Host Email:</td><td><a href=\"mailto:" + HostController.Instance.GetString("HostEmail") + "\">" + HostController.Instance.GetString("HostEmail") + "</a></td></tr>");
+			str_debug.AppendLine("<tr><td>Host URL:</td><td>" + HostController.Instance.GetString("HostURL") + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Friendly URLs:</td><td>" + HostController.Instance.GetString("UseFriendlyUrls") + "</td></tr>");
+			str_debug.AppendLine("<tr><td colspan=\"2\"><hr></td></tr>");
+			str_debug.AppendLine("<tr><td>Portal Title:</td><td>" + PortalSettings.PortalName + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Portal Email:</td><td><a href=\"mailto:" + PortalSettings.Email + "\">" + PortalSettings.Email + "</a></td></tr>");
+			str_debug.AppendLine("<tr><td>Portal Alias:</td><td>" + PortalSettings.PortalAlias.HTTPAlias + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Portal ID:</td><td>" + PortalSettings.PortalAlias.PortalID.ToString() + "</td></tr>");
+				
+			if (ModuleID > 0 & TabID > 0) 
+			{
+				ModuleController obj_module = new ModuleController();
+				DesktopModuleInfo obj_module_info = DesktopModuleController.GetDesktopModule(ModuleID, PortalId);
+				Hashtable obj_module_settings = new Hashtable();
+				obj_module_settings = obj_module.GetModuleSettings(ModuleID);
+
+				str_debug.AppendLine("<tr><td colspan=\"2\"><hr></td></tr>");
+				str_debug.AppendLine("<tr><td>Module Name:</td><td>" + obj_module_info.FriendlyName + "</td></tr>");
+				str_debug.AppendLine("<tr><td>Module Version:</td><td>" + obj_module_info.Version + "</td></tr>");
+				foreach (DictionaryEntry obj_module_setting in obj_module_settings)
+				{
+					str_debug.AppendLine("<tr><td>" + obj_module_setting.Key.ToString().Trim() + "</td><td>" + obj_module_setting.Value.ToString().Trim() + "</td></tr>");
+				}
+				str_debug.AppendLine("<tr><td colspan=\"2\"><hr></td></tr>");
+			}
+
+			str_debug.AppendLine("<tr><td colspan=\"2\"><hr></td></tr>");
+			str_debug.AppendLine("<tr><td>Application Directory:</td><td>" + DotNetNuke.Common.Globals.ApplicationPath + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Application Path:</td><td>" + DotNetNuke.Common.Globals.ApplicationMapPath + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Host Directory:</td><td>" + DotNetNuke.Common.Globals.HostPath + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Host Path:</td><td>" + DotNetNuke.Common.Globals.HostMapPath + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Portal Directory:</td><td>" + PortalSettings.HomeDirectory + "</td></tr>");
+			str_debug.AppendLine("<tr><td>Portal Path:</td><td>" + PortalSettings.HomeDirectoryMapPath + "</td></tr>");
+			str_debug.AppendLine("<tr><td colspan=\"2\"><hr></td></tr>");
+			str_debug.AppendLine("</table>");
+				
+			System.Web.HttpContext.Current.Response.Write(str_debug.ToString() + "<br />");
+		}
+
+	}
+
+
+
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DONEIN.NET Module Info Library")]
+[assembly: AssemblyDescription("Module Info Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DONEIN.NET")]
+[assembly: AssemblyProduct("DONEIN.NET Module Info Library")]
+[assembly: AssemblyCopyright("Copyright © 2012 DONEIN.NET.  All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba43976e-2c6e-4e7d-be4d-af875f79a2c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
